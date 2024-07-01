8cff40fddd2b9c272b30f645dab184eaa7ea5004
jiwoo424 <jiwoomail@naver.com>
2024-07-01 20:35:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -138,7 +138,6 @@ if my_expander.button("Recommend"):
         if len(unique_names) == 5:
             break
             
-    rest2idx = ALS.rest2idx
     related = als_model.similar_items(rest2idx[unique_names[0]])
     
     array2list = related[0]
